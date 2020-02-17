489d0db6b3fbddb2e6af0b043a712a42a55869d7
icrdr <icrdr2010@gmail.com>
2020-02-17 12:25:06 +0800

    备案链接

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
-import { Layout, Menu, Typography, Row, Col, Icon } from 'antd';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { Layout, Menu, Typography, Row, Col, Icon } from "antd";
 
-import { GalleryPage, LabPage } from './pages/mainPage';
+import { GalleryPage, LabPage } from "./pages/mainPage";
 
-import { menus } from './data.json';
+import { menus } from "./data.json";
 
-const { Text } = Typography
+const { Text } = Typography;
 const { Header, Content, Footer } = Layout;
 
-
 function App() {
-  const [nav, setNav] = useState('0');
-  const menuUI = menus.map((item, index) =>
-    <Menu.Item key={index} >
-      <NavLink to={item.link} isActive={(match, location) => {
-        if (match) {
-          setNav(index.toString())
-        }
-      }}>
+  const [nav, setNav] = useState("0");
+  const menuUI = menus.map((item, index) => (
+    <Menu.Item key={index}>
+      <NavLink
+        to={item.link}
+        isActive={(match, location) => {
+          if (match) {
+            setNav(index.toString());
+          }
+        }}
+      >
         <Icon type={item.icon} />
         <span>{item.title}</span>
       </NavLink>
     </Menu.Item>
-  )
+  ));
 
   return (
     <Router>
@@ -31,21 +33,22 @@ function App() {
         <Header className="p:0">
           <Row>
             <Col xs={0} md={2} className="t-a:c" style={{ width: 120 }}>
-              <Text className="f:2 f-w:500" style={{ color: '#fff' }} >马南</Text>
+              <Text className="f:2 f-w:500" style={{ color: "#fff" }}>
+                马南
+              </Text>
             </Col>
             <Col span={20}>
               <Menu
                 theme="dark"
                 mode="horizontal"
-                defaultSelectedKeys={['0']}
+                defaultSelectedKeys={["0"]}
                 selectedKeys={[nav]}
-                style={{ lineHeight: '64px' }}
+                style={{ lineHeight: "64px" }}
               >
                 {menuUI}
               </Menu>
             </Col>
           </Row>
-
         </Header>
         <Content className="p-t:1">
           <Row type="flex" justify="center">
@@ -54,13 +57,13 @@ function App() {
             <Route path="/lab" component={LabPage} />
           </Row>
         </Content>
-        <Footer className="t-a:c"><div>1-mu.net ©2018 Created by icrdr</div><div>备案号：陕ICP备16015882号-1</div></Footer>
+        <Footer className="t-a:c">
+          <a href="http://beian.miit.gov.cn">备案号：陕ICP备16015882号-1</a>
+        </Footer>
       </Layout>
       {/* <TopBar title="icrdr" link="./" />
 
       <MenuSide variant='permanent' /> */}
-
-
     </Router>
   );
 }
